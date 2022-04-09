285d9920e406c41c2d45e81bdb94e26507e8c646
Peter Andresen <peter@flank.dk>
2022-04-09 18:05:19 +0200

    Fixed spelling errors

--- src/scenes/harbor.tsx
+++ src/scenes/harbor.tsx
@@ -9,9 +9,9 @@ const Harbor = () => {
 		<h1>Harbor</h1>
 		<p>The Harbor is hosted in what seems like to be the lower decks of the station.</p>
 		<p>It host no living areas. Only offices, storages, workshops, and pubs.</p>
-		<p>The workers moving around in the Harbor is of the hardworking type. Big musces and worn out cloth. Even the overseers and administrative persernal</p>
-		<p>In the work hours, stresset are busy of the working people, but in the evening, will the people start to gather in the the pubs and allyes for beer and dicegames.</p>
-		<p>If you keep your wits about you and stick to the main road, then this is a safe place. But be carefull in the back allyes or looking like an easy target at night.</p>
+		<p>The workers moving around in the Harbor is of the hardworking type. Big muscles and worn out cloth. Even the overseers and administrative personnel are big guys</p>
+		<p>In the work hours, streets are busy of the working people, but in the evening, will the people start to gather in the the pubs and alleys for beer and dice games.</p>
+		<p>If you keep your wits about you and stick to the main road, then this is a safe place. But be careful in the back alleys or looking like an easy target at night.</p>
 		<hr />
 		<button onClick={() => updateScene(sceneNames.harbor_pub)} >Check out the pubs</button>
 		<button >Look at the starships docked</button>
