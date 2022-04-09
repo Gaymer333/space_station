3af9a6697ca8276241bccb3ab80e8a8b26432185
Peter Andresen <peter@flank.dk>
2022-04-09 17:36:10 +0200

    update: Update bar drinking

--- src/scenes/harbor-pub.tsx
+++ src/scenes/harbor-pub.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { sceneNames } from '.'
-import { statActions, statKeys, useGlobalState } from '../GlobalStateProvider'
+import { StatActions, StatKeys, useGlobalState } from '../GlobalStateProvider'
 
 const HarborPub = () => {
 	const { updateScene, changeStat } = useGlobalState()
+
+	const drinkBeer = () => {
+		changeStat({
+			statKey: StatKeys.alcohol,
+			action: StatActions.add,
+			value: 10
+		})
+	}
 
 	return <>
 		<h1>Harbor Pubs</h1>
 		<p>You enter the pub...</p>
 		<hr />
 		<p>Beer - 20 Buck</p>
-		<button onClick={() => changeStat({ statKey: statKeys.alcohol, action: statActions.add, value: 10 })} >Buy and drink</button>
+		<button onClick={drinkBeer} >Buy and drink</button>
 		<hr />
 		<button onClick={() => updateScene(sceneNames.harbor)} >Leave</button>
 	</>
